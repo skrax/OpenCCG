1993e903b6d5cf6c6814c1e77e977e714fc0e3e4
Fabian Seidl <fabianseidl@fseidl.eu>
2023-04-09 12:43:14 +0200

    refact inputeventsystem

--- src/InputEventSystem.cs
+++ src/InputEventSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Godot;
 using OpenCCG.Core;
@@ -18,6 +19,7 @@ public partial class InputEventSystem : Node2D
     private Vector2 _dragOffset;
     private Card? _cardToDrag;
 
+
     public override void _Ready()
     {
         _line = GetChild<Line2D>(0);
@@ -27,73 +29,138 @@ public partial class InputEventSystem : Node2D
     {
         if (inputEvent.IsActionPressed(InputActions.SpriteClick))
         {
-            if (_state is InputState.Idle)
-            {
-                var cardBoard = EventSink.PointerDownCardBoard.MinBy(x => x.ZIndex);
-                if (cardBoard != null)
-                {
-                    var mousePosition = ((InputEventMouseButton)inputEvent).Position;
-                    _state = InputState.DraggingLine;
-                    Logger.Info<InputEventSystem>($"DragLine Start {cardBoard.GetInstanceId()}");
-                    _line.Points = new[] { _line.ToLocal(mousePosition), _line.ToLocal(mousePosition) };
-                }
-
-                var card = EventSink.PointerDownCards.MinBy(x => x.ZIndex);
-                if (card != null)
-                {
-                    var mousePosition = ((InputEventMouseButton)inputEvent).Position;
-                    _state = InputState.DraggingCard;
-                    _dragOffset = mousePosition - card.Position;
-                    _cardToDrag = card;
-                    card.ZIndex = 1;
-                }
-            }
+            OnSpriteClickedPressed(inputEvent);
         }
 
         if (inputEvent.IsActionReleased(InputActions.SpriteClick))
         {
-            switch (_state)
-            {
-                case InputState.DraggingLine:
-                {
-                    var cardBoard = EventSink.PointerUpCardBoard.MinBy(x => x.ZIndex);
-                    if (cardBoard != null)
-                    {
-                        Logger.Info<InputEventSystem>($"DragLine End {cardBoard.GetInstanceId()}");
-                    }
-
-                    _line.Points = System.Array.Empty<Vector2>();
-                    _state = InputState.Idle;
-                    break;
-                }
-                case InputState.DraggingCard:
-                    _cardToDrag!.ZIndex = 0;
-                    _cardToDrag!.PlayOrInvokeDragFailure();
-                    _cardToDrag = null;
-                    _dragOffset = Vector2.Zero;
-                    _state = InputState.Idle;
-                    break;
-            }
+            OnSpriteClickReleased();
         }
 
         if (inputEvent is InputEventMouseMotion mouseMotion)
         {
-            switch (_state)
-            {
-                case InputState.DraggingLine:
-                {
-                    var mousePosition = mouseMotion.Position;
-                    var first = _line.Points[0];
-                    _line.Points = new[] { first, _line.ToLocal(mousePosition) };
-                    break;
-                }
-                case InputState.DraggingCard:
-                    _cardToDrag!.Position = mouseMotion.Position - _dragOffset;
-                    break;
-            }
+            OnMouseMotion(mouseMotion);
         }
 
-
         EventSink.Drain();
+    }
+
+    private void OnSpriteClickReleased()
+    {
+        switch (_state)
+        {
+            case InputState.DraggingLine:
+            {
+                OnDragLineEnd();
+                break;
+            }
+            case InputState.DraggingCard:
+            {
+                OnDragCardEnd();
+                break;
+            }
+            case InputState.Idle:
+            {
+                break;
+            }
+            default:
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    private void OnSpriteClickedPressed(InputEvent inputEvent)
+    {
+        if (_state is not InputState.Idle) return;
+
+        var mousePosition = ((InputEventMouseButton)inputEvent).Position;
+        var cardBoard = EventSink.PointerDownCardBoard.MinBy(x => x.ZIndex);
+        if (cardBoard != null)
+        {
+            OnDragLineStart(cardBoard, mousePosition);
+            return;
+        }
+
+        var card = EventSink.PointerDownCards.MinBy(x => x.ZIndex);
+        if (card != null)
+        {
+            OnDragCardStart(card, mousePosition);
+        }
+    }
+
+    private void OnMouseMotion(InputEventMouseMotion mouseMotion)
+    {
+        switch (_state)
+        {
+            case InputState.DraggingLine:
+            {
+                OnDragLineUpdate(mouseMotion);
+                break;
+            }
+            case InputState.DraggingCard:
+            {
+                OnDragCardUpdate(mouseMotion);
+                break;
+            }
+            case InputState.Idle:
+            {
+                break;
+            }
+            default:
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    private void OnDragCardUpdate(InputEventMouseMotion mouseMotion)
+    {
+        _cardToDrag!.Position = mouseMotion.Position - _dragOffset;
+    }
+
+    private void OnDragLineUpdate(InputEventMouseMotion mouseMotion)
+    {
+        var mousePosition = mouseMotion.Position;
+        var first = _line.Points[0];
+        _line.Points = new[] { first, _line.ToLocal(mousePosition) };
+    }
+
+
+    private void OnDragLineStart(CardBoard cardBoard, Vector2 mousePosition)
+    {
+        _state = InputState.DraggingLine;
+        Logger.Info<InputEventSystem>($"DragLine Start {cardBoard.GetInstanceId()}");
+        _line.Points = new[] { _line.ToLocal(mousePosition), _line.ToLocal(mousePosition) };
+    }
+
+    private void OnDragLineEnd()
+    {
+        var cardBoard = EventSink.PointerUpCardBoard.MinBy(x => x.ZIndex);
+        if (cardBoard != null)
+        {
+            Logger.Info<InputEventSystem>($"DragLine End {cardBoard.GetInstanceId()}");
+        }
+
+        _line.Points = Array.Empty<Vector2>();
+        _state = InputState.Idle;
+    }
+
+    private void OnDragCardStart(Card card, Vector2 mousePosition)
+    {
+        _state = InputState.DraggingCard;
+        _dragOffset = mousePosition - card.Position;
+        _cardToDrag = card;
+        card.ZIndex = 1;
+    }
+
+
+    private void OnDragCardEnd()
+    {
+        _cardToDrag!.ZIndex = 0;
+        _cardToDrag!.PlayOrInvokeDragFailure();
+        _cardToDrag = null;
+        _dragOffset = Vector2.Zero;
+        _state = InputState.Idle;
     }
 }
