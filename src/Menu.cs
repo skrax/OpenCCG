cadc0fddad36f00b8aed68bd6e10b88ab2345311
Fabian Seidl <fabianseidl@fseidl.eu>
2023-06-12 16:11:31 +0200

    add menu stuff

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Godot;
 
 namespace OpenCCG;
@@ -6,15 +7,58 @@ public partial class Menu : Node
 {
     private static readonly PackedScene CardBrowserScene = GD.Load<PackedScene>("res://scenes/card-browser.tscn");
     private static readonly PackedScene MainScene = GD.Load<PackedScene>("res://scenes/main.tscn");
-    private Button _playButton, _cardsButton;
+    [Export] private Button _playButton, _cardsButton, _loadDeckButton;
+    [Export] private Label _deckNameLabel;
+    [Export] private FileDialog _fileDialog;
+    [Export] private CheckButton _usePasswordCheckButton;
+    [Export] private LineEdit _password;
+    private string? _deckPath;
+    private bool _inQueue;
 
     public override void _Ready()
     {
-        _playButton = GetChild<Button>(0);
-        _cardsButton = GetChild<Button>(1);
+        _playButton.Pressed += () =>
+        {
+            if (_inQueue)
+            {
+                _inQueue = false;
+                _playButton.Text = "Play";
 
-        _playButton.Pressed += () => { GetTree().ChangeSceneToPacked(MainScene); };
+                _loadDeckButton.Disabled = false;
+                _cardsButton.Disabled = false;
+                _usePasswordCheckButton.Disabled = false;
+                _password.Editable = true;
+
+                return;
+            }
+
+            if (_deckPath == null) return;
+            if (_usePasswordCheckButton.ButtonPressed && _password.Text == string.Empty) return;
+
+            _inQueue = true;
+            _playButton.Text = "Cancel Queue";
+            _loadDeckButton.Disabled = true;
+            _cardsButton.Disabled = true;
+            _usePasswordCheckButton.Disabled = true;
+            _password.Editable = false;
+
+            //GetTree().ChangeSceneToPacked(MainScene);
+        };
 
         _cardsButton.Pressed += () => { GetTree().ChangeSceneToPacked(CardBrowserScene); };
+
+        _deckNameLabel.Text = "No Deck Selected";
+        _deckNameLabel.AddThemeColorOverride("font_color", Colors.Red);
+
+        _loadDeckButton.Pressed += () => _fileDialog.Visible = true;
+        _fileDialog.FileSelected += SetDeck;
+    }
+
+    private void SetDeck(string path)
+    {
+        _deckPath = path;
+        _deckNameLabel.Text = Path.GetFileNameWithoutExtension(path);
+        _deckNameLabel.AddThemeColorOverride("font_color", Colors.Green);
+        _playButton.Disabled = false;
     }
 }
